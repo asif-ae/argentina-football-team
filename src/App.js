a95298e30ba43a9e0acffd3aae0ecafd6cca534b
Asif Estiak <me.asif.ae@gmail.com>
2021-02-28 23:50:14 +0600

    imported bootstrap in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* Header Section */}
+      <header className="header container-fluid">
+        <h1>The project</h1>
       </header>
+      {/* /Header Section */}
     </div>
   );
 }
